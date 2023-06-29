8866a89bba95320d666837898b538e2f4f64fbd2
Rodrigo Garcia <rgarciaa125@gmail.com>
2023-06-29 14:51:12 -0300

    creacion del controlador de usuarios

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function Create(Request $request){
+
+    }
+
+    public function Read(Request $request){
+
+    }
+
+    public function Update(Request $request){
+
+    }
+
+    public function Delete(Request $request, $id){
+
+    }
+}
