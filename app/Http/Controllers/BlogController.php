dae5208be7611386f083eab9ff6ebd4a920fe0a4
Rodrigo Garcia <rgarciaa125@gmail.com>
2023-06-29 14:50:59 -0300

    creacion del controlador de blogs

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+
+class BlogController extends Controller
+{
+    public function Create(Request $request){
+
+    }
+
+    public function Read(Request $request){
+
+    }
+
+    public function Update(Request $request){
+
+    }
+
+    public function Delete(Request $request, $id){
+
+    }
+
+}
