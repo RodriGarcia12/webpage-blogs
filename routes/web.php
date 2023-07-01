4482467f7a155ccb8336651223057ba0e7fbe72a
Rodrigo Garcia <rgarciaa125@gmail.com>
2023-06-30 23:21:19 -0300

    agregado de rutas, login, sign up, etc

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/home', function() { 
+    return view('home'); 
+});
+Route::get('/login', function() {
+    return view('login');
+});
+Route::get('/logout', function() {
+    return view('logout');
+});
+Route::get('/signup', function() {
+    return view('signup');
+});
+
+Route::post('/loginUser', [UserController::class, 'Login']);
+Route::post('/createUser', [UserController::class, 'Create']);
