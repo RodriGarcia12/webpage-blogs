2a600604cc00188c44c1fdb1cec67ec3ca02eb38
Rodrigo Garcia <rgarciaa125@gmail.com>
2023-06-30 23:20:38 -0300

    creacion de header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>Free Blogs</h1>
+    <a href="/login">Iniciar sesion</a>
+    <a href="/logout">Cerrar sesion</a>
+    <a href="/signup">Crear usuario</a>
+    <a href="/config">Perfil</a>
+    <a href="/blog">Publicar</a>
+    <br>
+    <br>
+</div>
