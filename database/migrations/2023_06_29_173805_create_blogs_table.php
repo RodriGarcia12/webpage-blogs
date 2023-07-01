aeceefb753849013ebcbd58eb0806cca48709a60
Rodrigo Garcia <rgarciaa125@gmail.com>
2023-07-01 12:22:28 -0300

    cambio de string a mediumText el dato text

--- database/migrations/2023_06_29_173805_create_blogs_table.php
+++ database/migrations/2023_06_29_173805_create_blogs_table.php
@@ -17,7 +17,7 @@ class CreateBlogsTable extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('text');
+            $table->mediumText('text');
             $table->timestamps();
             $table->softDeletes();
         });
