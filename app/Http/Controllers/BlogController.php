9bf43dc90d1fb855d0a1f9964c9fa6d297f629a8
Rodrigo Garcia <rgarciaa125@gmail.com>
2023-07-01 12:21:26 -0300

    funcionalidad de creacion y listado terminadas

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,11 +8,22 @@ use App\Models\Blog;
 class BlogController extends Controller
 {
     public function Create(Request $request){
+        $b = new Blog();
 
+        $b -> title = $request -> post('title');
+        $b -> text = $request -> post('text');
+        
+        $b -> save();
+
+        return redirect('/home')->with('blogCreated', true);
     }
 
     public function Read(Request $request){
+        $b = Blog::all();
 
+        return view('home',[
+            'blogs' => $b
+        ]);
     }
 
     public function Update(Request $request){
