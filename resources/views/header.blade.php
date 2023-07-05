9b9ecfc7ae5475e606fd36a76885185ea3a9adae
Rodrigo Garcia <rgarciaa125@gmail.com>
2023-07-04 22:23:04 -0300

    creacion if auth para mostrar links

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,11 +1,17 @@
 <div>
     <h1>Free Blogs</h1>
-    <a href="/home">Inicio</a>
-    <a href="/login">Iniciar sesion</a>
+@if(Auth::check())
+    <b>Bienvenido {{ Auth::user() -> name }}</b>
+@endif
+    <a href="/">Inicio</a>
+@if(Auth::check())
     <a href="/logout">Cerrar sesion</a>
-    <a href="/signup">Crear usuario</a>
     <a href="/config">Perfil</a>
     <a href="/blog">Publicar</a>
+@else
+    <a href="/login">Iniciar sesion</a>
+    <a href="/signup">Crea tu usuario</a>
+@endif
     <br>
     <br>
 </div>
